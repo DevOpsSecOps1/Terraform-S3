f2ecde77a12c849e5bb53e8731b22fc76679101a
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-06 23:20:34 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = “AKIAXZRFGMTHQFLGIDBB”
+}
+variable "aws_secret_key" {
+default = “+F5XdWwbzuSQs/w3BAnX0KHAwmhZ6oJJPOb51qo6”
+ }
+variable "region" {
+    default = "us-east-1"
+}
