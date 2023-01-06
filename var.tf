e7ba3cc8d88771d2d8f386bbf6fae6f557bd9ec5
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-06 23:49:38 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,5 +1,16 @@
-variable "bucket_name" {}
+#variable "bucket_name" {}
 
-variable "acl_value" {
-    default = "private"
+#variable "acl_value" {
+#    default = "private"
+#}
+variable "bucket_name" {
+  description = "mygvenkats3bucket2794"
+  type        = string
 }
+
+variable "tags" {
+  description = "release1236"
+  type        = map(string)
+  default     = {}
+}
+
