43cc9d6e001696697e376b320ff83d169542b807
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-06 23:30:36 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-default = AKIAXZRFGMTHQFLGIDBB
+default = "AKIAXZRFGMTHQFLGIDBB"
 }
 variable "aws_secret_key" {
-default = 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
+default = "+F5XdWwbzuSQs/w3BAnX0KHAwmhZ6oJJPOb51qo6"
  }
 variable "region" {
-    default = us-east-1
+    default = "us-east-1"
 }
