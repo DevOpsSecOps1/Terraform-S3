36e458ecdd8f2b871a50d2e9cc83fe434647c7c4
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-06 23:42:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,7 @@ provider "aws" {
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket-318"
-  bucket_name = "venkatmybucket9484128"
+  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
