b74832f2e0b0236d1e8b34a855e141024c084e23
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-06 23:47:35 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,13 @@ default = "+F5XdWwbzuSQs/w3BAnX0KHAwmhZ6oJJPOb51qo6"
 variable "region" {
     default = "us-east-1"
 }
+variable "bucket_name" {
+  description = "mygvenkats3bucket2794"
+  type        = string
+}
+
+variable "tags" {
+  description = "release1236"
+  type        = map(string)
+  default     = {}
+}
