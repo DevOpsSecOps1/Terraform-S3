dedcce176027ace081a7046159532f105564add5
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-06 23:40:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ provider "aws" {
     region = "${var.region}"
 }
 
-module "s3" {
-    source = "terraform-aws-modules/s3-bucket/aws"
-    #bucket name should be unique
-    bucket_name = "venkatmybucket9484128"       
-}
-#module "s3_bucket" {
-#  source = "terraform-aws-modules/s3-bucket/aws"
-
-#  bucket = "my-s3-bucket"
-#  acl    = "private"
-
- # versioning = {
- #   enabled = true
- # }
-
+#module "s3" {
+#    source = "terraform-aws-modules/s3-bucket/aws"
+#    #bucket name should be unique
+#    bucket_name = "venkatmybucket9484128"       
 #}
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-318"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
